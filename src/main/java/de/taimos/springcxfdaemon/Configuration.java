1b4d36ef78b41584e09b46041d483620db532230
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2013-11-08 21:12:38 +0100

    new jetty fixes #4

--- src/main/java/de/taimos/springcxfdaemon/Configuration.java
+++ src/main/java/de/taimos/springcxfdaemon/Configuration.java
@@ -18,6 +18,7 @@ public interface Configuration {
 	public static final String PROFILES_PRODUCTION = "prod";
 	public static final String PROFILES_TEST = "test";
 	
+	public static final String DEFAULT_HANDLER_CLASS = "defaultHandlerClass";
 	public static final String SERVICE_PACKAGE = "servicePackage";
 	
 	public static final String SERVICE_PORT = "svc.port";
