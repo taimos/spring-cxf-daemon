08ea1387c92d32223820ee22fc3a3d4d09fb4a43
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2015-02-18 21:59:41 +0100

    add RestDoc Feature with Annotation Scan

---
+++ src/main/java/de/taimos/springcxfdaemon/ServiceAnnotationClassesProvider.java
@@ -0,0 +1,47 @@
+package de.taimos.springcxfdaemon;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.restdoc.cxf.provider.IClassesProvider;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ServiceAnnotationClassesProvider implements IClassesProvider {
+	
+	private Class<? extends Annotation> serviceAnnotation;
+	
+	private ListableBeanFactory beanFactory;
+	
+	
+	public ListableBeanFactory getBeanFactory() {
+		return this.beanFactory;
+	}
+	
+	@Autowired
+	public void setBeanFactory(ListableBeanFactory beanFactory) {
+		this.beanFactory = beanFactory;
+	}
+	
+	public Class<? extends Annotation> getServiceAnnotation() {
+		return this.serviceAnnotation;
+	}
+	
+	public void setServiceAnnotation(Class<? extends Annotation> serviceAnnotation) {
+		this.serviceAnnotation = serviceAnnotation;
+	}
+	
+	@Override
+	public Class<?>[] getClasses() {
+		Map<String, Object> beansWithAnnotation = this.beanFactory.getBeansWithAnnotation(this.serviceAnnotation);
+		List<Class<?>> classes = new ArrayList<>();
+		for (Object bean : beansWithAnnotation.values()) {
+			classes.add(bean.getClass());
+			classes.addAll(Arrays.asList(bean.getClass().getInterfaces()));
+		}
+		return classes.toArray(new Class[0]);
+	}
+}
