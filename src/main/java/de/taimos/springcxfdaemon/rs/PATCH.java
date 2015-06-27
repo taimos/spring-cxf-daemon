fdc00b32ca83a4c2b823c7a0b33acbb438789f8f
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2015-06-27 23:18:07 +0200

    fix #11 add @PATCH

---
+++ src/main/java/de/taimos/springcxfdaemon/rs/PATCH.java
@@ -0,0 +1,23 @@
+package de.taimos.springcxfdaemon.rs;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.ws.rs.HttpMethod;
+
+/**
+ * Indicates that the annotated method responds to HTTP PATCH requests.
+ *
+ * @author Thorsten Hoeger
+ * @see HttpMethod
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@HttpMethod("PATCH")
+@Documented
+public @interface PATCH {
+	//
+}
